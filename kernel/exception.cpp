407590bdc7bdc839ad44172346b195e8ce4ee53b
wzj <jcdomt@163.com>
2026-09-19 16:10:48 +0800

    rm(exception): 删除部分无用代码

--- kernel/exception.cpp
+++ kernel/exception.cpp
@@ -4,33 +4,9 @@
 #include <driver/uart/uart.h>
 #include <kernel/exception.h>
 
-// 获取异常原因
-static inline unsigned long read_esr_el1() {
-    unsigned long value;
-    asm volatile("mrs %0, esr_el1" : "=r"(value));
-    return value;
-}
-
-// 获取异常保存位置
-static inline unsigned long read_elr_el1() {
-    unsigned long value;
-    asm volatile("mrs %0, elr_el1" : "=r"(value));
-    return value;
-}
-
-// 获取异常状态寄存器
-static inline unsigned long read_spsr_el1() {
-    unsigned long value;
-    asm volatile("mrs %0, spsr_el1" : "=r"(value));
-    return value;
-}
 
 extern "C" void exception_sync_handler(ExceptionContext *context) {
     uart_puts("Sync Exception!\n");
-
-    // unsigned long esr = read_esr_el1();
-    // unsigned long elr = read_elr_el1();
-    // unsigned long spsr = read_spsr_el1();
 
     uart_puts("ESR_EL1: ");
     uart_puthex(context->esr);
