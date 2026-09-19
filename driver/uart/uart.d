5038becd72b9a61c8b821c4e8e1a88688c175ea5
wzj <jcdomt@163.com>
2026-09-19 16:07:51 +0800

    rm(file): 删除编译产物

--- driver/uart/uart.d
+++
@@ -1,2 +0,0 @@
-uart.o: uart.cpp /root/work/XuYuanOS/include/driver/uart/uart.h
-/root/work/XuYuanOS/include/driver/uart/uart.h:
--- main.d
+++
@@ -1,2 +0,0 @@
-main.o: main.cpp /root/work/XuYuanOS/include/driver/uart/uart.h
-/root/work/XuYuanOS/include/driver/uart/uart.h:
